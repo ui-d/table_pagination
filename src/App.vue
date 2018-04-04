e7220f1d0377f8d0f209c89aac786c231282dc4c
unknown <dawiddeveloper@gmail.com>
2018-04-04 16:01:31 +0200

    major changes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Output</router-link> |
+      <router-link to="/about">Input</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+img {
+  width: 10%;
+  padding: 20px;
+}
 </style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,197 @@
+<template>
+  <div class="app">
+    <table class="app__table">
+      <thead>
+        <tr v-for="(header, index) in headers" :key="index">
+          <td>{{header.first}}</td>
+          <td>{{header.second}}</td>
+          <td>{{header.third}}</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(detail, index) in details" :key="index">
+          <td>{{detail.task}}</td>
+          <td>{{detail.priority}}</td>
+          <td>
+            <input type="checkbox" name="" id="" v-model="detail.done">
+          </td>
+        </tr>
+        <tr class="app__table-control">
+          <td colspan="3">Rows per page:
+            <select name="" id="" v-model="pagination_nr">
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+            </select>
+            {{pagination_nr}} {{details.length}}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        headers: [{
+          first: "Task name",
+          second: "Priority",
+          third: "Done"
+        }],
+        details: [{
+            task: "Take out the trash",
+            priority: "Medium",
+            done: false
+          },
+          {
+            task: "Feed the dog",
+            priority: "High",
+            done: true
+          },
+          {
+            task: "Order lunch",
+            priority: "High",
+            done: false
+          },
+          {
+            task: "Send email to Peter",
+            priority: "Low",
+            done: true
+          },
+          {
+            task: "Buy groceries",
+            priority: "Medium",
+            done: false
+          },
+          {
+            task: "Take out the trash",
+            priority: "Medium",
+            done: false
+          },
+          {
+            task: "Feed the dog",
+            priority: "High",
+            done: true
+          },
+          {
+            task: "Order lunch",
+            priority: "High",
+            done: false
+          },
+          {
+            task: "Send email to Peter",
+            priority: "Low",
+            done: true
+          },
+          {
+            task: "Buy groceries",
+            priority: "Medium",
+            done: false
+          },
+          {
+            task: "Take out the trash",
+            priority: "Medium",
+            done: false
+          },
+          {
+            task: "Feed the dog",
+            priority: "High",
+            done: true
+          },
+          {
+            task: "Order lunch",
+            priority: "High",
+            done: false
+          },
+          {
+            task: "Send email to Peter",
+            priority: "Low",
+            done: true
+          },
+          {
+            task: "Buy groceries",
+            priority: "Medium",
+            done: false
+          }
+        ],
+        pagination: [
+        
+        ],
+        pagination_nr: ""
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  /* Table of contents
+=====================
+// 1. Font Faces
+// 2. Base
+// 3. Layout
+// 4. Block + element
+// 5. Modifier
+// 6. State
+// 7. Animations
+=====================
+*/
+
+  $pure-white: #ffffff;
+  $wood-brown: #494430;
+  $sweet-orange: #F79933;
+  $hole-dark: #525252;
+  $light-gray: #EEEEF0;
+  $middle-gray: #d6d6d6;
+  $hell-dark: #1D1D1D;
+
+  // 1. Font Faces
+  // 2. Base
+  .app {
+    min-width: 100%;
+    background: $pure-white;
+    font-size: 62.5%;
+    color: $hole-dark;
+  }
+
+  // 3. Layout
+  // 4. Block + element
+  .app__table {
+    width: 670px;
+    margin: 0 auto 100px auto;
+    font-size: 1rem;
+    text-align: left;
+    border: 1px solid $middle-gray;
+    box-shadow: 0 0 0 7px $light-gray;
+  }
+
+  thead {
+    background: $wood-brown;
+    color: $pure-white;
+    td {
+      border: none;
+    }
+  }
+
+  td {
+    height: 47px;
+    padding-left: 15px;
+  }
+
+  .app__table-control {
+    text-align: right;
+  }
+
+select {
+  border: none;
+  color: $hole-dark;
+  font-size: 1rem;
+  width: 65px;
+}
+
+  // 5. Modifier
+  // 6. State
+  // 7. Animations
+</style>
